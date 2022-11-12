8eb60d144434291dc7118b745d79cc18637ddbd7
shakerAbuDrais <thecoffeiner@gmail.com>
2022-11-12 12:46:06 +0300

    Fix the styling based on review

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,19 +3,23 @@ import { Link, Route, Routes } from 'react-router-dom';
 import './Header.css';
 import Books from './Books';
 import Categories from './Categories';
+import profileImg from './profile_image.png';
 
 const header = () => (
   <div>
     <nav className="header-nav">
-      <h1 className="title"> Book Store CMS </h1>
-      <ul className="nav-ul">
-        <li>
-          <Link to="/"> Books </Link>
-        </li>
-        <li className="mid-li">
-          <Link to="/Categories"> Categories </Link>
-        </li>
-      </ul>
+      <div className="header">
+        <h1 className="title"> Book Store CMS </h1>
+        <ul className="nav-ul">
+          <li>
+            <Link to="/"> Books </Link>
+          </li>
+          <li className="mid-li">
+            <Link to="/Categories"> Categories </Link>
+          </li>
+        </ul>
+      </div>
+      <img className="profile-image" src={profileImg} alt="Profile" width="50" height="50" />
     </nav>
     <Routes>
       <Route path="/" element={<Books />} />
